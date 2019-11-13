c50a56f701eb04607ea5f0520546143809babefc
royyan1207070 <royyan120707@gmail.com>
2019-11-13 09:22:07 +0700

    apla 0.1

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react'
 import { Text, View } from 'react-native'
 
+import MenJa from './component/MenJa'
+
 export default class App extends Component{
   render(){
     return(
-      <View>
-        <Text>tau ah</Text>
-      </View>
+    <MenJa/>
     )
   }
 }
