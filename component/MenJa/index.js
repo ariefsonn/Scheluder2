0f70de71d8bf746ee408d7b1653ff55d72e4a2f1
royyan1207070 <royyan120707@gmail.com>
2019-11-14 10:36:24 +0700

    beta 0.1

--- component/MenJa/index.js
+++ component/MenJa/index.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react'
-import { Text, View } from 'react-native'
+import { Text, View, TextInput, TouchableOpacity, ScrollView, Image } from 'react-native'
 
-export default class App extends Component{
+export default class MenJa extends Component{
   render(){
     return(
-      <View>
-        <Text>tau ah</Text>
-      </View>
+      <ScrollView>
+        <View style={{ padding: 32 }}>
+            <Image source={ require('../../assets/sche.jpg') } style={{ height: 180, width: 180, alignSelf: 'center', margin: 20}} />
+
+           <TextInput placeholder="Tentukan Jadwal" keyboardType="default" style={{ padding: 12, fontSize: 12, borderWidth: 2, borderRadius: 4, borderColor: 'purple', margin: 8 }} /> 
+
+           <TouchableOpacity>
+             <View style={{  padding: 18, margin: 7, marginTop: 17, backgroundColor: 'purple', alignItems: 'center', borderRadius: 8 }}>
+               <Text style={{ color: 'white', fontSize: 10,  }}>Tambah Jadwal</Text>
+             </View>
+           </TouchableOpacity>
+        </View>
+      </ScrollView>
     )
   }
 }
